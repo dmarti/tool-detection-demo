3e8d6976130c707fba4946c2f1d7242fdccd26b4
Don Marti <dmarti@zgp.org>
2019-03-08 09:27:58 -0800

    Add demo script and CSS

---
+++ demo.js
@@ -0,0 +1,38 @@
+function logMessage(msg) {
+	console.log(msg);
+	var messages = document.getElementById('messages');
+	var el = document.createElement('p');
+	el.innerHTML = msg;
+	messages.appendChild(el);
+}
+
+function googlePresent() {
+	// true if Google Analytics is present
+	if (window.ga && ga.loaded) {
+		return true;
+	}
+	logMessage("Google Analytics not found on this page.");
+	return false;
+}
+
+function sendEvent(eventName) {
+	if(!googlePresent()) {
+		return;
+	}
+	logMessage("Sending event to GA: " + eventName);
+	ga('send', {
+		hitType: 'event',
+		eventCategory: 'aloodo',
+		eventAction: eventName
+	});
+}
+
+function setup(ev) {
+	logMessage("demo script loaded.");
+	if(typeof aloodo === 'object') {
+		aloodo.onLoad(function() {sendEvent("loaded")});
+		aloodo.onDetected(function() {sendEvent("tracking")});
+	}
+}
+
+window.addEventListener("load", setup);
